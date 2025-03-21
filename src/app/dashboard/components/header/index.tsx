b77bc5109094f1b9e7d462dd2d74498affd1d385
John Santos <jhow.mito11@gmail.com>
2025-03-21 15:37:50 -0300

    Funçao de deslogar completo

--- src/app/dashboard/components/header/index.tsx
+++ src/app/dashboard/components/header/index.tsx
@@ -1,9 +1,22 @@
+"use client"
+
 import Link from 'next/link'
 import styles from './styles.module.scss'
 import Image from 'next/image'
 import logoImg from '/public/Logo.svg'
+import { LogInIcon } from 'lucide-react'
+import { deleteCookie } from 'cookies-next'
+import { useRouter } from 'next/navigation'
 
 export function Header() {
+  const router = useRouter()
+
+  async function handleLogout (){
+    deleteCookie('login',{path: '/'})
+
+    router.replace('/')
+
+  }
   return (
     <header className= {styles.headerContainer}>
        <div className= {styles.headerContent}>
@@ -27,6 +40,18 @@ export function Header() {
             <Link href={'/dashboard/product'}>
             Produtos
             </Link>
+
+            <form action={handleLogout} >
+                <button 
+                type='submit'
+                >
+                    <LogInIcon
+                    size={24}
+                    color={'#fff'}
+                    />
+                </button>
+
+            </form>
 
         </nav>
 
