c979bf2daf3f4ceb64660a8a0ea87bece55d9b58
John Santos <jhow.mito11@gmail.com>
2025-03-25 09:53:49 -0300

    Funcionalidade de cadastrar produto

--- src/app/dashboard/product/components/form/index.tsx
+++ src/app/dashboard/product/components/form/index.tsx
@@ -5,10 +5,78 @@ import styles from './styles.module.scss'
 import { UploadCloud } from 'lucide-react'
 import Image from 'next/image'
 import { Button } from '@/app/dashboard/components/button'
-export function Form() {
+import {api} from '@/services/api'
+import { getCookieClient } from '@/lib/cookieClient'
+
+
+
+interface CategoryProps{
+    id: string;
+    name: string
+}
+
+interface Props{
+    categories: CategoryProps[]
+}
+
+export function Form({categories}: Props) {
 
     const [image, setImage] =useState<File >()
     const [previewImage, setPreviewImage] = useState<string >()
+
+    async function handleRegisterProduct(formData: FormData){
+        console.log('Chamooou')
+
+        const categoryIndex = formData.get('category')
+        const name = formData.get('name')
+        const price = formData.get('price')
+        const description = formData.get('description')
+
+        if(!categoryIndex || !name || !price || !description || !image){
+            return
+        }
+
+       const data = new FormData()
+
+       console.log({
+        name,
+        price,
+        description,
+        category: categories[Number(categoryIndex)].id,
+        image
+      });
+
+       data.append("name", name)
+       data.append("price", price)
+       data.append("description", description)
+       data.append("category_id", categories [Number(categoryIndex)].id)
+       data.append("file", image)
+
+
+
+       const token =  getCookieClient()
+
+       console.log('Token:', token);
+
+       try {
+        const response = await api.post('/product', data, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        
+        console.log('Resposta:', response.data)
+        alert('Produto cadastrado com sucesso!')
+    } catch (error: any) {
+        console.error('Erro detalhado:', error)
+        alert(`Erro ao cadastrar produto: ${error.response?.data?.message || error.message}`)
+    }
+
+console.log('Produto cadastrado com sucesso')
+
+}
+
 
     function handleFile(e:ChangeEvent<HTMLInputElement>){
         
@@ -16,7 +84,7 @@ export function Form() {
             const image = e.target.files[0]
 
             if(image.type !== 'image/jpeg' && image.type !== 'image/png'){
-                alert('Apenas arquivos PNG e JPEG são aceitos')
+                console.log('Apenas arquivos PNG e JPEG são aceitos')
                 return
 
             }
@@ -32,7 +100,9 @@ export function Form() {
         <main className={styles.container}>
             <h1> Novo produto</h1>
 
-            <form className={styles.form}>
+            <form className={styles.form}
+            action={handleRegisterProduct}
+            >
                 <label className={styles.labelImage}>
                     <span>
                         <UploadCloud size={30} color='#fff'/>
@@ -59,25 +129,34 @@ export function Form() {
 
                 </label>
 
-                <select name='category' className={styles.select}>
-                    <option key={1} value='1'>Pizzas</option>
-                    <option key={1} value='1'>Massas</option>
+                <select name='category' 
+                className={styles.select}>
+                    {categories.map((category,index) => (
+                    <option key={category.id} value={index}>
+                    {category.name}
+                    </option>
+                                ))}
+              
+                  
                 </select>
 
                 <input 
                 type='text' 
+                name='name'
                 placeholder='Digite o nome do produto' 
                 required
                 className={styles.input} 
                 />
                 <input 
                 type='text' 
+                name='price'
                 placeholder=' Digite o preço' 
                 required
                 className={styles.input} 
                 />
                 <textarea 
                 placeholder='Digite a descrição' 
+                name='description'
                 required
                 className={styles.input} 
                 />
--- src/app/dashboard/product/page.tsx
+++ src/app/dashboard/product/page.tsx
@@ -1,9 +1,27 @@
 import { Form } from "./components/form";
+import {api} from '@/services/api'
+import { getCookieServer } from '@/lib/cookieServer';
 
-export default function Product () {
+export default async function Product () {
+
+    const token = await getCookieServer()
+
+    console.log('Token:', token) // Verifique se o token está correto
+
+
+    
+    const response = await api.get('/category', {
+
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    console.log(response.data)
+
     return (
         <main>
-        <Form/>
+        <Form categories = { response.data}/>
         </main>
     )
 }
